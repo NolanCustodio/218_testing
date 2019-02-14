0603aadbab0f0d41ef54cee7596e535649494324
Mike <mike.nichols97@gmail.com>
2019-02-13 20:27:04 -0500

    Added two tests to the testing file. One tests for Strings and the other tests for Booleans.

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelnichols
+ * Date: 2019-02-13
+ * Time: 18:58
+ */
+
+declare(strict_types=1);
+
+$name = "John Doe";
+$number = 200;
+$bool = TRUE;
