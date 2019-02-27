0214a5aaa56950b1c73d4f04038c931d46adf85f
Mike <mike.nichols97@gmail.com>
2019-02-27 18:29:03 -0500

    Added .csv data file, File Class and the FileTest Class

---
+++ src/File.php
@@ -0,0 +1,26 @@
+<?php
+
+class File
+{
+    public static function readCSVtoArray(String $filename, String $class):array
+    {
+        $records = Array();
+        $count = 0;
+        $fieldNames = '';
+
+        if (($handle = fopen($filename, "r")) !== FALSE) {
+            while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+
+                if($count == 0) {
+                    $fieldNames = $row;
+                } else {
+                    $records[] = (object) array_combine($fieldNames, $row);
+                }
+                $count++;
+            }
+            fclose($handle);
+        }
+
+        return $records;
+    }
+}
