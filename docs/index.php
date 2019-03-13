8a53746aec5d2d3b2d516bbabe52b91187705eb5
Mike <mike.nichols97@gmail.com>
2019-03-12 23:00:57 -0400

    Feature: Added navbar information and started building the Record class

--- docs/index.php
+++ docs/index.php
@@ -1,35 +1,42 @@
-<!doctype html>
-<html class="no-js" lang="">
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>IS 218 | Mini Project 1</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+              integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    </head>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
+    <body>
 
-<body>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+            <a class="navbar-brand" href="https://github.com/mnichols17/218_testing">IS 218 Project 1</a>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <a class="navbar-brand" href="#">IS 218</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://mikenichols.xyz">Michael Nichols</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nolan Custodio</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-</nav>
+        <main role="main" class="container">
+            <div>
+                <table class="table table-striped table-bordered">
+                    <?php
+                        require("../src/File.php");
+                    ?>
+                </table>
+            </div>
+        </main>
 
-<main role="main" class="container">
-    <div>
-        <table class="table table-striped table-bordered">
-            <?php
-
-            ?>
-        </table>
-    </div>
-
-</main>
-
-</body>
-
+    </body>
 </html>
